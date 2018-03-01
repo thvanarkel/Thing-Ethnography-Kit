9fdc24e41d622a717dec9ce16e94dcf2ca6bbd38
thvanarkel <me@thomasvanarkel.nl>
2018-03-01 17:51:46 +0000

    Removed GrovePi code

--- Software/MasterCamera/mastercamera.py
+++ Software/MasterCamera/mastercamera.py
@@ -1,16 +1,7 @@
 from picamera import PiCamera
 from time import sleep
 
-import time
-import grovepi
-
-numleds = 4
-ledPin = 7
-
-
 # SETUP
-grovepi.pinMode(ledPin, "OUTPUT");
-grovepi.chainableRgbLed_init(ledPin, numleds)
 
 camera = PiCamera()
 camera.resolution = (1920,1280)
@@ -20,7 +11,4 @@ camera.framerate = 15
 
 for i in range(5):
     sleep(5)
-    grovepi.chainableRgbLed_test(ledPin, numleds, 7)
     camera.capture('/home/pi/Desktop/image%s.jpg' % i)
-    sleep(0.5)
-    grovepi.chainableRgbLed_test(ledPin, numleds, 0)
