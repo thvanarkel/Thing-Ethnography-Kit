f268a63da3b8502a1758f4a3ddf7c661aae44e4f
thvanarkel <me@thomasvanarkel.nl>
2018-03-03 21:26:26 +0000

    test

--- Software/MasterCamera/mastercamera.py
+++ Software/MasterCamera/mastercamera.py
@@ -18,6 +18,7 @@ from remi import start, App
 
 # PROGRAM
 
+<<<<<<< Updated upstream
 class MasterCamera(App):
     def __init__(self, *args):
         super(MasterCamera, self).__init__(*args)
@@ -59,3 +60,19 @@ start(MasterCamera ,address='127.0.0.1', port=8082, multiple_instance=True,enabl
 #         camera.capture('/home/pi/Desktop/images/image%s.jpg' % photoCount)
 #         photoCount = photoCount + 1
 #         sleep(2)
+=======
+def data_received_callback(xbee_message):
+    address = xbee_message.remote_device.get_64bit_addr()
+    data = xbee_message.data
+    global sensorValue
+    sensorValue = int.from_bytes(data, byteorder='big')
+    print ("Received data from %s: %s" % (address, sensorValue))
+
+device.add_data_received_callback(data_received_callback)
+
+while 1:
+    if sensorValue > 300:
+        camera.capture('/home/pi/Desktop/images/image%s.jpg' % photoCount)
+        photoCount = photoCount + 1
+        sleep(2)
+>>>>>>> Stashed changes
