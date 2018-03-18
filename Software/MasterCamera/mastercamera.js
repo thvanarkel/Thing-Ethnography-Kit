e2810e18083d49b12b01982acce779731812ca7c
thvanarkel <me@thomasvanarkel.nl>
2018-03-18 23:24:19 +0100

    Working version of node.js server

--- Software/MasterCamera/mastercamera.js
+++ Software/MasterCamera/mastercamera.js
@@ -1,7 +1,8 @@
 var http = require('http').createServer(handler);
 var fs = require('fs');
+var Xbee = require('digimesh');
 
-http.listen(8080);
+http.listen(8888);
 
 function handler(req, res) {
   fs.readFile(__dirname + '/templates/main.html', function(err, data) {
@@ -15,7 +16,7 @@ function handler(req, res) {
   })
 }
 
-var xbee = new Xbee({ device: '/dev/ttyU0', baud: 115200 }, function() {
+var xbee = new Xbee({ device: '/dev/ttyAMA0', baud: 9600 }, function() {
     console.log('xbee is ready');
 
     console.log('getting node identifier...');
@@ -23,32 +24,13 @@ var xbee = new Xbee({ device: '/dev/ttyU0', baud: 115200 }, function() {
     xbee.get_ni_string(function(err, data) {
         if (err) return console.err(err);
         console.log("my NI is '" + data.ni + "'");
-
         console.log('scanning for nodes on the network...');
         xbee.discover_nodes(function(err, nodes) {
             if (err) return console.err(err);
             console.log('%d nodes found:', nodes.length);
             console.dir(nodes);
 
-            // if we found anyone
-            if (nodes.length) {
-                console.log("saying 'hello' to node %s...", nodes[0].addr);
-                xbee.send_message({
-                    data: new Buffer("hello"),
-                    addr: nodes[0].addr,
-                    broadcast: false,
-                },
-                // callback
-                function(err, data) {
-                    if (err) return console.error(err);
-                    // print the string status message for the status we got back
-                    console.log('delivery status: %s',
-                        xbee.DELIVERY_STATUS_STRINGS[data.status]);
-                    console.dir(data);
-                    console.log('goodbye');
-                    process.exit(0);
-                });
-            }
+            
         });
     });
 });
@@ -59,6 +41,6 @@ xbee.on('error', function(err) {
 });
 
 xbee.on('message_received', function(data) {
-    console.log('received a message!');
-    console.dir(data);
+	console.log('received a message!');
+	console.dir(data);
 });
