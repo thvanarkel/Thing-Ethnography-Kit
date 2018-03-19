4247d0ff0801f04ab597a32b02801c97b5e605a4
thvanarkel <me@thomasvanarkel.nl>
2018-03-19 18:20:06 +0100

    Basic websocket implementation

--- Software/MasterCamera/mastercamera.js
+++ Software/MasterCamera/mastercamera.js
@@ -1,10 +1,13 @@
-var http = require('http').createServer(handler);
+var http = require('http');
 var fs = require('fs');
 var Xbee = require('digimesh');
 
-http.listen(8888);
+var ipaddress = 'localhost';
+var port = 8888;
 
-function handler(req, res) {
+var wss = require('ws').Server;
+
+var server = http.createServer(function(req, res) {
   fs.readFile(__dirname + '/templates/main.html', function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
@@ -14,7 +17,33 @@ function handler(req, res) {
     res.write(data);
     return res.end();
   })
-}
+});
+
+server.listen(port, function() {
+	console.log((new Date()) + 'Server is listening on port ' + port);
+});
+
+var ws = new wss({
+	server: server
+});
+
+ws.on('connection', function(s) {
+	console.log('New connection');
+	s.on('message', function(m) {
+		console.log('received: %s', m);
+		s.send('reply from server : ' + m);
+	});
+	s.send('something');
+});
+
+ws.broadcast = function broadcast(data) {
+	ws.clients.forEach(function each(client) {
+		console.log(wss.OPEN);
+		if (client.readyState === 1) {
+			client.send(data);
+		}
+	});
+};
 
 var xbee = new Xbee({ device: '/dev/ttyAMA0', baud: 9600 }, function() {
     console.log('xbee is ready');
@@ -29,8 +58,6 @@ var xbee = new Xbee({ device: '/dev/ttyAMA0', baud: 9600 }, function() {
             if (err) return console.err(err);
             console.log('%d nodes found:', nodes.length);
             console.dir(nodes);
-
-            
         });
     });
 });
@@ -43,4 +70,5 @@ xbee.on('error', function(err) {
 xbee.on('message_received', function(data) {
 	console.log('received a message!');
 	console.dir(data);
+	ws.broadcast('{' + data.addr + ', ' + String((data.data[0] * 256) + data.data[1]) + '}');
 });
