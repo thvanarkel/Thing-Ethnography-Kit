9a091371f8a19e020ca079a9cc045ba7bc9f8b9e
thvanarkel <me@thomasvanarkel.nl>
2018-03-25 16:29:14 +0000

    Xbee reading

--- Software/Camera/camera.js
+++ Software/Camera/camera.js
@@ -1,4 +1,9 @@
 var http = require('http');
+var fs = require('fs');
+var Xbee = require('digimesh');
+
+
+
 
 var ipaddress = 'localhost';
 var port = 8888;
@@ -9,4 +14,15 @@ var server = http.createServer(function(req, res) {
 });
 server.listen(port, function() {
 	console.log((new Date()) + 'Server is listening on port ' + port);
+});
+
+var xbee = new Xbee({ device: '/dev/ttyAMA0', baud: 9600 }, function() {
+    console.log('xbee is ready');
+
+    console.log('getting node identifier...');
+    // ask for node identifier string
+    xbee.get_ni_string(function(err, data) {
+        if (err) return console.err(err);
+        console.log("my NI is '" + data.ni + "'");
+    });
 });
